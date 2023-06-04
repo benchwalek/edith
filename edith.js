98db882bbcd980078f8e18fd7598014296e766ad
Ben Chwalek <ben.chwalek@rwth-aachen.de>
2023-06-05 00:30:15 +0200

    add bayer dithering

--- edith.js
+++ edith.js
@@ -56,8 +56,37 @@ let dither_kernels = {
 	 [ 2,2,1/42]],
 }
 
+let bayer_matrices = { "bayer1": new bayerTexture( 4,  Uint8Array.of(0,128,32,160,192,64,224,96,48,176,16,144,240,112,208,80)),
+					   "bayer2": new bayerTexture( 8,  Uint8Array.of(0,128,32,160,8,136,40,168,192,64,224,96,200,72,232,104,48,176,16,144,56,184,24,152,240,112,208,80,248,120,216,88,12,140,44,172,4,132,36,164,204,76,236,108,196,68,228,100,60,188,28,156,52,180,20,148,252,124,220,92,244,116,212,84)),
+					   "bayer3": new bayerTexture( 16, Uint8Array.of(0,128,32,160,8,136,40,168,2,130,34,162,10,138,42,170,192,64,224,96,200,72,232,104,194,66,226,98,202,74,234,106,48,176,16,144,56,184,24,152,50,178,18,146,58,186,26,154,240,112,208,80,248,120,216,88,242,114,210,82,250,122,218,90,12,140,44,172,4,132,36,164,14,142,46,174,6,134,38,166,204,76,236,108,196,68,228,100,206,78,238,110,198,70,230,102,60,188,28,156,52,180,20,148,62,190,30,158,54,182,22,150,252,124,220,92,244,116,212,84,254,126,222,94,246,118,214,86,3,131,35,163,11,139,43,171,1,129,33,161,9,137,41,169,195,67,227,99,203,75,235,107,193,65,225,97,201,73,233,105,51,179,19,147,59,187,27,155,49,177,17,145,57,185,25,153,243,115,211,83,251,123,219,91,241,113,209,81,249,121,217,89,15,143,47,175,7,135,39,167,13,141,45,173,5,133,37,165,207,79,239,111,199,71,231,103,205,77,237,109,197,69,229,101,63,191,31,159,55,183,23,151,61,189,29,157,53,181,21,149,255,127,223,95,247,119,215,87,253,125,221,93,245,117,213,85))}
+
+
+function bayerTexture(size, array)
+{
+	this.size = size
+	this.array = array
+}
+
+bayerTexture.prototype.at = function(x, y) {
+	x %= this.size
+	y %= this.size
+	return this.array[y*this.size+x]
+}
+
 var original_image = null
 
+function bayer_dither(pixels, threshold_map) {
+	for (var row = 0; row < pixels.height; row++) {
+		for (var col = 0; col < pixels.width; col++) {
+			var color = pixels.getPixel(col, row, 0)
+
+			if (color > threshold_map.at(col, row))
+				pixels.setPixel(col, row, 255)
+			else
+				pixels.setPixel(col, row, 0)
+		}
+	}
+}
 
 
 window.onload = function () {
@@ -112,21 +141,20 @@ function process() {
 	ctx.drawImage(original_image, 0, 0, new_width, new_height)
 	image_data = ctx.getImageData(0, 0, new_width, new_height)
 
-	// for (var i = 0; i < image_data.data.length; i+=4)
-	// 	image_data.data[i] = image_data.data[i+1] = image_data.data[i+2] = (image_data.data[i]
-	// 																	  + image_data.data[i+1]
-	// 																	  + image_data.data[i+2])/3
+	var method = document.getElementById("method_dropdown").value
 
-	kernel = dither_kernels[document.getElementById("method_dropdown").value]
 
 	pixels = new MonochromePixelData(image_data, new_width, new_height)
 	pixels.inplaceMap = function (f) { 
 								for (var i = 0; i < this.data.length; i++)
 									this.data[i] = f(this.data[i])
-							}
+						}
 
 	pixels.inplaceMap(srgbToLinear)
-	dither(pixels, kernel)
+	if (method.includes("bayer"))
+		bayer_dither(pixels, bayer_matrices[method])
+	else
+		dither(pixels, dither_kernels[method])
 	pixels.inplaceMap(linearToSrgb)
 
 	ctx.putImageData(pixels.asImageData(),0,0)
